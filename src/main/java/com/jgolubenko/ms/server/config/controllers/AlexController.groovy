4ef0ee28776c50a866444f5922cb9d09e325eaaa
golubenko_y <yevgen.java@gmail.com>
2015-12-12 00:27:15 -0800

    update

---
+++ src/main/java/com/jgolubenko/ms/server/config/controllers/AlexController.groovy
@@ -0,0 +1,24 @@
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AlexController
+{
+  @RequestMapping("/alex")
+  public @ResponseBody Map<String, Object> test()
+  {
+    Map<String, Object> resp = new HashMap<>();
+    resp.put("This is key", new Date());
+    
+    return resp;
+  }
+  
+  @RequestMapping("/alex2")
+  public @ResponseBody Map<String, Object> alex2()
+  {
+    ["this is key" : new Date()]
+  }
+}
